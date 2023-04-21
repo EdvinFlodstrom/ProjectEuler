11a59b1ed3dfab09c0ba0337fb2dec2cfc6cf5cf
EdvinFlodstrom <118813992+EdvinFlodstrom@users.noreply.github.com>
2023-04-21 09:12:14 +0200

    Add project files.

---
+++ ProjectEuler/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Uppgift_1();
+
+        }
+        static void Uppgift_1()
+        {
+
+        }
+    }
+}
